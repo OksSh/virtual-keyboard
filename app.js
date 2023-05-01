6d159c61e1af53d86b709f65b7ab84511a8f38fa
Oksana Shkulepa <o.shkulepa@gmail.com>
2023-05-01 23:52:22 +0300

    feat: add class active for button

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 const keyCodes = [
   192, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189, 187, 8, 9, 81, 87, 69, 82,
   84, 89, 85, 73, 79, 80, 219, 221, 220, 20, 65, 83, 68, 70, 71, 72, 74, 75, 76,
-  186, 222, 13, 16, 90, 88, 67, 86, 66, 78, 77, 188, 190, 191, 16, 17, 91, 18,
-  32, 18, 17, 37, 38, 40, 39,
+  186, 222, 13, 16, 90, 88, 67, 86, 66, 78, 77, 188, 190, 38, 16, 17, 91, 18,
+  32, 18, 17, 37, 40, 39, 191,
 ];
 const keys = [
   '`',
@@ -57,7 +57,7 @@ const keys = [
   'M',
   ',',
   '.',
-  '/',
+  '&uarr;',
   'Shift',
   'Ctrl',
   'Meta',
@@ -66,24 +66,23 @@ const keys = [
   'Alt',
   'Ctrl',
   '&larr;',
-  '&uarr;',
   '&darr;',
   '&rarr;',
+  '/',
 ];
-
 let keyboard = document.createElement('div');
 let textArea = document.createElement('textarea');
 
 //Add elements
 textArea.className = 'text-area';
 document.body.append(textArea);
-
 keyboard.className = 'keyboard';
 document.body.append(keyboard);
 
 for (let i = 0; i < keys.length; i++) {
   let button = document.createElement('div');
   button.className = 'button';
+  button.id = keyCodes[i];
   if (keys[i] == ' ') {
     button.classList.add('button-space');
   }
@@ -95,3 +94,29 @@ for (let i = 0; i < keys.length; i++) {
 }
 
 //Listeners
+keyboard.addEventListener('click', (event) => {
+  if (event.target.classList.contains('button')) {
+    event.target.classList.toggle('active');
+    setTimeout(() => event.target.classList.toggle('active'), 200);
+  }
+});
+
+document.addEventListener('keydown', (event) => {
+  event.preventDefault();
+  let buttons = document.querySelectorAll('.button');
+  for (let item of buttons) {
+    if (item.id == event.which) {
+      item.classList.toggle('active');
+    }
+  }
+});
+
+document.addEventListener('keyup', (event) => {
+  event.preventDefault();
+  let buttons = document.querySelectorAll('.button');
+  for (let item of buttons) {
+    if (item.id == event.which) {
+      item.classList.toggle('active');
+    }
+  }
+});
